666fcc47f55fc1077b9b7e9a643cff1940b0025f
Rahemanali-24 <159017790+Rahemanali-24@users.noreply.github.com>
2024-04-05 16:25:21 +0530

    mid

--- backend/src/middlewares/auth.mid.ts
+++ backend/src/middlewares/auth.mid.ts
@@ -8,7 +8,7 @@ export default (req: any, res: any, next: any) => {
     if(!token) return res.status(HTTP_UNAUTHORIZED).send();
 
     try {
-        const decodedUser = verify(token, 'SomeRandomText');
+        const decodedUser = verify(token, process.env.JWT_SECRET!);
         req.user = decodedUser;
 
     } catch (error) {
